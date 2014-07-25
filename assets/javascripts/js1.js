c43b88553091183e83dc94c1180f4488accbde3b
Ozzie3000 <ozzieboy19@hotmail.com>
2014-07-25 19:22:52 -0400

    js on click

--- assets/javascripts/js1.js
+++ assets/javascripts/js1.js
@@ -1,4 +1,11 @@
+function getStuff () {
+	//  document.querySelector('#tuna')  //this will target first one it finds  //
+	document.querySelector('#tuna').onclick=talk;
+}
+function talk () {
+	alert('Chicken of the Sea!!!');
+}
+// next you must call it when you load document  //
+window.onload=getStuff;
 
-
-
- //  25-   working with JS   //
+ //  25-   working with JS with building my own functions   //
