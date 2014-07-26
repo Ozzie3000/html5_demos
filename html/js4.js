77867f5fcf02e1ad085a23049162986feda873e3
Ozzie3000 <ozzieboy19@hotmail.com>
2014-07-26 14:08:47 -0400

    iframe and basic html5

--- html/js4.js
+++ html/js4.js
@@ -1,5 +1,18 @@
-js4.js
+function getPisces () {
+	// querySelectorAll looks for all  //
+	var list1 = document.querySelectorAll('#.big_wrapper');
+	for (var i=0; i<list1.length; i++){
+		list1[i].onclick=popup1;    // an array [i] = index  //
+	}
+}
+function popup1 () {
+	alert('Chicken of the Sea!!!');
+}
 
+
+window.onload=getPisces;
+
+// no tunas yet //
 
 
 
