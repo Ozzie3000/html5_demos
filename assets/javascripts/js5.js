3ac0db7334f9fed25bde2d6b631ede6af9cb20af
Ozzie3000 <ozzieboy19@hotmail.com>
2014-07-27 10:56:40 -0400

    vars in js

--- assets/javascripts/js5.js
+++ assets/javascripts/js5.js
@@ -1,1 +1,14 @@
-js5.js
+function function_name (argument) {
+	// body...
+	TagName=document.getElementsByTagName('')
+}
+
+function doFirst (){
+	barSize=600;
+	samplevid=document.getElementsById('samplevid');
+	playButton=document.getElementsById('playButton');
+	defaultBar=document.getElementsById('defaultBar');
+	progressBar=document.getElementsById('progressBar');
+
+	playButton.addEventListener('','','');
+}
